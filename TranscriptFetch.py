43bd39d7f7a353e8a6742bb3af0ff67fde22e80f
Saksham Shrey <sakshamshrey_work@icloud.com>
2025-01-19 00:55:16 +0530

    Update TranscriptFetch.py

--- TranscriptFetch.py
+++ TranscriptFetch.py
@@ -123,7 +123,7 @@ def get_captions():
             concatenated_text = " ".join(
                 text["text"] for text in parsed_captions if text["text"]
             )
-            concatenated_text = concatenated_text.replace("&#39;", "'")
+            concatenated_text = concatenated_text.replace("&#39;", " ; ")
 
             return jsonify({
                 "video_id": video_id,
